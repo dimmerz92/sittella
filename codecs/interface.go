03954fe48283ccb1967e1d3a65f2bd4dba6881c0
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2025-07-09 10:32:53 +0800

    add mailer interface

--- codecs/interface.go
+++ codecs/interface.go
@@ -3,8 +3,8 @@ package codecs
 // Codec defines the interface for encoding implementations.
 type Codec interface {
 	// Marshal encodes the given data and returns it as a byte slice.
-	Marshal(any) ([]byte, error)
+	Marshal(data any) ([]byte, error)
 
 	// Unmarshal decodes the given byte slice into the destination pointer.
-	Unmarshal([]byte, any) error
+	Unmarshal(data []byte, dest any) error
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/dimmerz92/sittella
 
 go 1.24.4
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/wneessen/go-mail v0.6.2
+)
+
+require (
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
